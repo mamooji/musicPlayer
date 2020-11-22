7c373080b54e56451d7a8be9444f0b5c2f090a2e
mamooji <mamoojim@hotmail.com>
2020-11-22 13:45:58 -0500

    started working on a light / dark theme functionality

--- src/App.js
+++ src/App.js
@@ -7,13 +7,30 @@ import Library from "./Components/Library";
 import "./Styles/app.scss";
 //Utility import
 import Data from "./util";
+//icon import
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function App() {
   //state
   const [songs, setSongs] = useState(Data);
   const [currentSong, setCurrentSong] = useState(songs[6]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [theme, setTheme] = useState("light");
+  const themeHandler = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
   return (
     <div className="App">
+      <FontAwesomeIcon
+        className="theme-icon"
+        icon={theme === "light" ? faSun : faMoon}
+        onClick={themeHandler}
+        size="2x"
+      />
       <Song song={currentSong} />
       <Player
         song={currentSong}
